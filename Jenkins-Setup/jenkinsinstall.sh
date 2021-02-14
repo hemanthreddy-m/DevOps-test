3d5addf7c739471503c72f77a74c35f9c258edbe
Chinna <kanakanti98@gmail.com>
2021-02-14 18:37:51 +0530

    final code

--- Jenkins-Setup/jenkinsinstall.sh
+++ Jenkins-Setup/jenkinsinstall.sh
@@ -1,28 +1,27 @@
 
-#Installing JAVA on JENKINS SERVER
+#!/bin/bash
+
+echo "Installing Ansible"
+sudo yum -y install ansible
+sudo yum install git
+
+echo "Installing JAVA"
 sudo yum update -y
 sudo yum install java-1.8.0-openjdk-devel
 
-
-#Installing Jenkins APPLICATION
+echo "Installing Jenkins APPLICATION"
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
 
-
-#CONFIGURE JENKINS SERVER 
+echo "CONFIGURING JENKINS SERVER"
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
 
-#Install Ansible on Jenkins
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install ansible
 
-#Install git client on Jenkins server
-sudo yum install git
 
-#https://labs.tadigital.com/index.php/2018/08/02/how-to-run-ansible-playbooks-on-ec2-without-key-pairs/
+
 
 
 
